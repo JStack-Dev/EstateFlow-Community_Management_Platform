f8a0457e49c3e492f108d74e91be753c0f6f6055
Jorge Juan <jorgemoscosochacon@gmail.com>
2026-04-01 05:55:05 +0200

    fix: base path github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-
-  // 🔥 CLAVE: Vite usa "mode"
-  base:
-    mode === "production"
-      ? "/EstateFlow-Community_Management_Platform/"
-      : "/",
-}));
+  base: "/EstateFlow-Community_Management_Platform/", // 👈 CLAVE
+});
