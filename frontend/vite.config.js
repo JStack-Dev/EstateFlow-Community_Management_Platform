d63ea39abc411cb43515c74283b8c81f859c57e5
Jorge Juan <jorgemoscosochacon@gmail.com>
2026-05-03 12:17:44 +0200

    fix vite config vercel final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/EstateFlow-Community_Management_Platform/",
+  // 🔥 PARA VERCEL (no usar el de GitHub Pages)
+  base: "/",
+
+  build: {
+    rollupOptions: {
+      external: ["fsevents"], // 🔥 evita el error en Vercel
+    },
+  },
 });
