9002e4a1d799fecbfdd20c07e1200d013ab7bb94
Jorge Juan <jorgemoscosochacon@gmail.com>
2026-05-03 12:46:12 +0200

    fix deploy pipeline

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,22 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  base: "/",
-
-  resolve: {
-    alias: {
-      fsevents: false, // 🔥 BLOQUEA totalmente el módulo
-    },
-  },
-
-  optimizeDeps: {
-    exclude: ["fsevents"], // 🔥 evita que Vite lo procese
-  },
-
-  build: {
-    rollupOptions: {
-      external: ["fsevents"], // 🔥 seguridad extra
-    },
-  },
+  base: "/EstateFlow-Community_Management_Platform/"
 });
