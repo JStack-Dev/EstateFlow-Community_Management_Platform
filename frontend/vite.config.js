58edc4bce2d62f138d390a840261e090bd0a7a52
Jorge Juan <jorgemoscosochacon@gmail.com>
2026-05-03 21:09:00 +0200

    fix: externalize fsevents for CI build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/EstateFlow-Community_Management_Platform/",
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      external: ["fsevents"],
+    },
+  },
 });
