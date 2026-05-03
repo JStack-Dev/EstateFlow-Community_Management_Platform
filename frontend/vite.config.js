69fa03f96ba1438a34cd3c645cfc65e5b76442a8
Jorge Juan <jorgemoscosochacon@gmail.com>
2026-05-03 20:54:36 +0200

    deploy: fix build (omit optional deps)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/EstateFlow-Community_Management_Platform/",
   plugins: [react()],
-
-  build: {
-    target: "esnext",
-  },
-
-  resolve: {
-    alias: {
-      path: "path-browserify",
-    },
-  },
-
-  optimizeDeps: {
-    exclude: ["path"],
-  },
 });
