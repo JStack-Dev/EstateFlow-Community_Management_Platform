bb4301b7a52ee3e13b37a545d33405b72bb606cc
Jorge Juan <jorgemoscosochacon@gmail.com>
2026-05-03 22:19:16 +0200

    fix: rebuild frontend clean (rollup bug solved)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,25 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/EstateFlow-Community_Management_Platform/",
   plugins: [react()],
-
-  define: {
-    global: "window",
-  },
-
-  resolve: {
-    conditions: ["browser"],
-  },
-
-  optimizeDeps: {
-    exclude: ["fsevents"],
-  },
-
-  build: {
-    rollupOptions: {
-      external: ["fsevents"],
-    },
-    commonjsOptions: {
-      ignoreTryCatch: false,
-    },
-  },
 });
