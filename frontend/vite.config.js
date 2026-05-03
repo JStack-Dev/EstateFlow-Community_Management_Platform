4526cb4f21e7831ab8ca4243be11299bfe4856be
Jorge Juan <jorgemoscosochacon@gmail.com>
2026-05-03 21:11:24 +0200

    fix: prevent node modules in browser bundle

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,24 @@ export default defineConfig({
   base: "/EstateFlow-Community_Management_Platform/",
   plugins: [react()],
 
+  define: {
+    global: "window",
+  },
+
+  resolve: {
+    conditions: ["browser"],
+  },
+
+  optimizeDeps: {
+    exclude: ["fsevents"],
+  },
+
   build: {
     rollupOptions: {
       external: ["fsevents"],
+    },
+    commonjsOptions: {
+      ignoreTryCatch: false,
     },
   },
 });
